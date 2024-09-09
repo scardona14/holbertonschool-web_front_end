1ab5e864159f963afd50b7d10920d89cdbafeac5
scardona14 <cardanasean123@gmail.com>
2024-09-09 18:28:44 -0400

    Private methods with closure

---
+++ Javascript_advanced/6-hogwarts.js
@@ -0,0 +1,45 @@
+// Define the class studentHogwarts
+function studentHogwarts() {
+    // Variables
+     let privateScore = 0;
+     let name = null;
+
+    // Private method
+function changeScoreBy(points) {
+    privateScore += points;
+ }
+
+ return {
+    setName: function (newName) {
+        name = newName;
+    },
+
+    rewardStudent: function () {
+        changeScoreBy(1);
+    },
+
+    penalizeStudent: function () {
+        changeScoreBy(-1);
+    },
+
+    getScore: function () {
+        return `${name}: ${privateScore}`;
+    },
+  };
+}
+
+const harry = new studentHogwarts();
+harry.setName("Harry");
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+harry.rewardStudent();
+console.log(harry.getScore());
+
+const draco = new studentHogwarts();
+draco.setName("Draco");
+draco.rewardStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+draco.penalizeStudent();
+console.log(draco.getScore());
